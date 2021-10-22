4148c43c25d523cfc770ccf846197b654dcdf6d1
thefurorjuror <ranjanmail.rohit@gmail.com>
2021-10-22 23:34:31 +0530

    pretrained cnn model has replaced tesseract

--- masker.py
+++ masker.py
@@ -18,6 +18,57 @@ parser.add_argument('--output_folder', default='./output/', type=str, help='fold
 
 args = parser.parse_args()
 
+import torch
+from torch import nn
+from torch.optim import lr_scheduler
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import LogSoftmax
+from torch import flatten
+
+class LeNet(Module):
+
+    def __init__(self, numChannels=3):
+        super(LeNet, self).__init__()
+        self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+            kernel_size=(3, 3))
+        self.relu1 = ReLU()
+        self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2 = Conv2d(in_channels=20, out_channels=50,
+            kernel_size=(3, 3))
+        self.relu2 = ReLU()
+        self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.fc1 = Linear(in_features=50*11*11 , out_features=512)
+        self.relu3 = ReLU()
+        self.fc2 = Linear(in_features=512, out_features=20)
+        self.relu4 = ReLU()
+        self.fc3 = Linear(in_features=20, out_features=1)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        x = flatten(x,1)
+        
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        x = self.relu4(x)
+
+        x = self.fc3(x)
+        output = torch.sigmoid(x)  
+
+        return output
+
 def dist(a,b,c,d):
   return np.sqrt((a-c)**2 + (b-d)**2)
 
@@ -47,7 +98,14 @@ if __name__ == '__main__':
     os.mkdir(args.output_folder)
   except:
     pass
-  
+  if(args.cuda==False):
+    device='cpu'
+  else:
+    device='cuda'
+  model = LeNet()
+  model.load_state_dict(torch.load("./cnn_model",map_location=torch.device(device)))
+  model = model.to(device)
+  model.eval()
   pbar = tqdm(paths)
   for IMAGE_PATH in pbar:
     num_list = []
@@ -69,20 +127,21 @@ if __name__ == '__main__':
             rect = cv2.boundingRect(points) # returns (x,y,w,h) of the rect
             crop_image = res[rect[1]: rect[1] + rect[3], rect[0]: rect[0] + rect[2]]
             try:
-              crop_image= cv2.cvtColor(crop_image, cv2.COLOR_BGR2GRAY)
+              crop_image=cv2.resize(crop_image,(50,50))
             except:
               continue
-            string = pytesseract.image_to_string(crop_image,lang='eng',config='--psm 6')
+            crop_image=crop_image.transpose(2,0,1)
+            crop_image=crop_image.reshape(1,3,50,50)
+            crop_image = torch.from_numpy(crop_image.astype(np.float32))
+            crop_image = crop_image.to(device)            
 
             x=2.15
             y=2.65
  
             if (((dist(a,b,c,d)/dist(c,d,e,f)>=1/y) and (dist(a,b,c,d)/dist(c,d,e,f)<=1/x)) ):        
               num_list.append((a,b,c,d,e,f,g,h))
-            elif (replace_chars(string)!= ""):
-              string=string.strip()
-              if (len(string)==4 or len(string)==12):
-                num_list.append((a,b,c,d,e,f,g,h))
+            elif (model(crop_image).item()>=0.5):
+              num_list.append((a,b,c,d,e,f,g,h))
     for (a,b,c,d,e,f,g,h) in num_list:
       points = np.array([[[a,b],[c,d],[e,f],[g,h]]])
       cv2.fillPoly(image, points, (0,0,0))
